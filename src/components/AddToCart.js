a662b192c347aec25a5f6ab13c35fc5d92a0a130
Aaron Bracken <joshmidla@gmail.com>
2021-10-31 20:23:11 -0500

    product plus and minus btn

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,8 +8,29 @@ import AmountButtons from './AmountButtons'
 const AddToCart = ({product}) => {
 
   const { id, stock, colors} = product;
-  // const [amount, setAmount] = useState(0)
   const [mainColor, setMainColors] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+
+  const increase = () => {
+    setAmount((oldAmount)=> {
+      let tempAmount = oldAmount + 1;
+      if(tempAmount > stock ){
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+  const decrease = () => {
+    setAmount((oldAmount)=> {
+      let tempAmount = oldAmount - 1;
+      if(tempAmount < 1 ){
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
+
 
   return (
     <Wrapper>
@@ -17,14 +38,26 @@ const AddToCart = ({product}) => {
         <span>colors : </span>
         <div>{
           colors.map((color, index)=> {
-            return <button key ={index} className='color-btn' style={{background: color}}>
-              {index}
+            return <button key ={index}
+             className={`${mainColor === color? 'color-btn active' : 'color-btn'}`}
+             style={{background: color}}
+             onClick={()=> setMainColors(color)}>
+              {mainColor === color? <FaCheck /> : null } 
+              
             </button>
 
           })
           }</div>
       </div>
-      <div className="btn-container"></div>
+      <div className="btn-container">
+          <AmountButtons
+           amount= {amount} 
+          increase= {increase} 
+          decrease= {decrease}/> 
+          <Link to='/cart' className='btn'>
+            add to cart
+          </Link>  
+      </div>
     </Wrapper>
     
     )
