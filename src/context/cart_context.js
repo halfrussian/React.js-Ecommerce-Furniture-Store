655e075015a61f658e95b2aa5049c68de941b255
Joshua Bracken <joshua.bracken@kingmetals.com>
2021-11-03 16:01:12 -0500

    cart done

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -6,6 +6,7 @@ import {
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
   COUNT_CART_TOTALS,
+  CLEAR_FILTERS,
 } from '../actions'
 
 
@@ -34,19 +35,31 @@ export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   //add to crt
-  const addToCart =(id, color, amount, product) => {
-    dispatch({type:ADD_TO_CART, payload:{id, color, amount, product}})
+  const addToCart =(id, color, amount, product, ) => {
+    dispatch({type:ADD_TO_CART, payload:{id, color, amount, product,}})
   }
   
   //remove item
-  const removeItem = (id) => {}
+  const removeItem = (id) => {
+    dispatch({type:REMOVE_CART_ITEM, payload:id})
+  }
 
   //toggle amount
-  const toggleAmount = (id, value) => {}
+  const toggleAmount = (id, value) => {
+    console.log(id, value)
+    dispatch({type:TOGGLE_CART_ITEM_AMOUNT, payload:{id,value}
+    })
+  }
 
   //clearcart 
-  const clearCart = ()=> {}
+  const clearCart = ()=> {
+    dispatch({type:CLEAR_CART})
+  }
+
+
+
 useEffect(()=> {
+  dispatch({type:COUNT_CART_TOTALS})
   localStorage.setItem('cart', JSON.stringify(state.cart))
 }, [state.cart])
 
