ac00b5d9168426905e16fe09dc8388f636f1abbe
Joshua Bracken <joshua.bracken@kingmetals.com>
2021-11-03 09:55:56 -0500

    fixing, last commit was from other project restablish main

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,7 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({product}) => {
-
+console.log('hello')
   const { id, stock, colors} = product;
   const [mainColor, setMainColors] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
